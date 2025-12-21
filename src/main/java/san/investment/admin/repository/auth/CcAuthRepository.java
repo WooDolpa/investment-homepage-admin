5cac9b94718f61b8dba308ea0b89db4bbe3642c6
jwlee <dannis1124@naver.com>
2025-12-21 22:46:54 +0900

    cc_auth 엔티티 관련 제거

--- src/main/java/san/investment/admin/repository/auth/CcAuthRepository.java
+++
@@ -1,7 +0,0 @@
-package san.investment.admin.repository.auth;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import san.investment.common.entity.auth.CcAuth;
-
-public interface CcAuthRepository extends JpaRepository<CcAuth, Integer> {
-}
